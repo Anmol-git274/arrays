dde91fc5c0b135d912bc8abd513124ac8aa0c126
Anmol-git274 <anmolshahu2027@gmail.com>
2026-08-12 21:07:11 +0530

    Add files via upload

---
+++ initialization of array in compile time.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+	int main(){
+		int array[6] = {13,44,533,6543,33335,54456978};
+		printf("%d,%d,%d,%d,%d,%d.",array[0],array[1],array[2],array[3],array[4],array[5]);
+		
+		return 0;
+	}
